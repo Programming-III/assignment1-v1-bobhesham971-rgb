55dcfc417f5ddfbb24b24e1c259bdb1a5f9eb848
bobhesham971-rgb <bobhesham971@gmail.com>
2025-11-12 15:16:43 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -3,10 +3,18 @@
 
 #include "Animal.h"
 
-public Animal
-    class Bird{
-        private: float wingSpan;
-
+class Bird : public Animal {
+private:
+    float wingSpan;
+public:
+    Bird() : Animal(), wingSpan(0.0f) {}
+    Bird(string n, int a, bool h, float w) : Animal(n, a, h), wingSpan(w) {}
+    ~Bird() {}
+    void display() {
+        Animal::display();
+        cout << endl;
+    }
+};
 
 
 
