6e6ab8bd5fbc59652e7680ffad11545196741742
bobhesham971-rgb <bobhesham971@gmail.com>
2025-11-12 15:16:06 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,10 +3,17 @@
 
 #include "Animal.h"
 
-  public Animal:
-    class Mammal{
-        private: string furColor;
+ class Mammal : public Animal {
+private:
+    string furColor;
+public:
+    Mammal() : Animal(), furColor("") {}
+    Mammal(string n, int a, bool h, string f) : Animal(n, a, h), furColor(f) {}
+    ~Mammal() {}
+    void display() {
+        Animal::display();
+        cout << endl;
     }
-
+};
 
 #endif
